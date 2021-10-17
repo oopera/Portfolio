6e35b143f998134418b134c806a8d825e795dce0
lucas? <66785409+oopera@users.noreply.github.com>
2021-10-17 18:19:49 +0200

    Update javascripts.js

--- Persönliche Homepage (Unfertig) HTML-CSS-JS-PHP-MYSQL/JS/javascripts.js
+++ Persönliche Homepage (Unfertig) HTML-CSS-JS-PHP-MYSQL/JS/javascripts.js
@@ -1,9 +1,3 @@
-function lickmyballs(variable){
-var str = 69
-document.getElementById("DOOKIE").innerHTML = str;
-
-}
-
 function changebacky(){
 	document.getElementById("body").style.backgroundImage="url(images/640601.jpg)";
 	
